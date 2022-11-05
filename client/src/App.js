05e4f4ea51dd037a9a7242c1c13373e9f9ccac38
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-11-05 20:04:25 +0300

    route handling reverting Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
 import Home from "../src/components/Home";
 import InterventionsViewDetails from "./components/InterventionsViewDetails";
 import RedFlagsViewDetails from "./components/RedFlagsViewDetails";
@@ -28,75 +27,73 @@ function App() {
 
   return (
     <div className="row mt-3">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          {user?.is_admin ? (
-            <Route
-              exact
-              path="/interventions/:interventionId"
-              element={<InterventionsViewDetails />}
-            />
-          ) : (
-            <Route
-              path="/interventions/:interventionId"
-              element={<NotAuthorized />}
-            />
-          )}
-          {user?.is_admin ? (
-            <Route
-              exact
-              path="/redflags/:redflagId"
-              element={<RedFlagsViewDetails />}
-            />
-          ) : (
-            <Route path="/redflags/:redflagId" element={<NotAuthorized />} />
-          )}
-          {user?.is_admin ? (
-            <Route
-              exact
-              path="/dashboard"
-              element={
-                <AdminDashboard
-                  user={user}
-                  redFlags={redFlags}
-                  setRedFlags={setRedFlags}
-                  interventions={interventions}
-                  setInterventions={setInterventions}
-                />
-              }
-            />
-          ) : (
-            <Route path="/dashboard" element={<NotAuthorized />} />
-          )}
-          <Route exact path="/login" element={<SignIn onLogin={setUser} />} />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        {user?.is_admin ? (
           <Route
             exact
-            path="/get-started"
-            element={<SignUp onLogin={setUser} />}
+            path="/interventions/:interventionId"
+            element={<InterventionsViewDetails />}
           />
-          {user ? (
-            <Route
-              exact
-              path="/user-landing"
-              element={<UserLanding user={user} />}
-            />
-          ) : (
-            <Route path="/user-landing" element={<NotAuthorized />} />
-          )}
-          {user?.is_admin ? (
-            <Route exact path="/users" element={<UsersList user={user} />} />
-          ) : (
-            <Route path="/users" element={<NotAuthorized />} />
-          )}
-          {user ? (
-            <Route exact path="/profile" element={<Profile user={user} />} />
-          ) : (
-            <Route path="/profile" element={<NotAuthorized />} />
-          )}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
+        ) : (
+          <Route
+            path="/interventions/:interventionId"
+            element={<NotAuthorized />}
+          />
+        )}
+        {user?.is_admin ? (
+          <Route
+            exact
+            path="/redflags/:redflagId"
+            element={<RedFlagsViewDetails />}
+          />
+        ) : (
+          <Route path="/redflags/:redflagId" element={<NotAuthorized />} />
+        )}
+        {user?.is_admin ? (
+          <Route
+            exact
+            path="/dashboard"
+            element={
+              <AdminDashboard
+                user={user}
+                redFlags={redFlags}
+                setRedFlags={setRedFlags}
+                interventions={interventions}
+                setInterventions={setInterventions}
+              />
+            }
+          />
+        ) : (
+          <Route path="/dashboard" element={<NotAuthorized />} />
+        )}
+        <Route exact path="/login" element={<SignIn onLogin={setUser} />} />
+        <Route
+          exact
+          path="/get-started"
+          element={<SignUp onLogin={setUser} />}
+        />
+        {user ? (
+          <Route
+            exact
+            path="/user-landing"
+            element={<UserLanding user={user} />}
+          />
+        ) : (
+          <Route path="/user-landing" element={<NotAuthorized />} />
+        )}
+        {user?.is_admin ? (
+          <Route exact path="/users" element={<UsersList user={user} />} />
+        ) : (
+          <Route path="/users" element={<NotAuthorized />} />
+        )}
+        {user ? (
+          <Route exact path="/profile" element={<Profile user={user} />} />
+        ) : (
+          <Route path="/profile" element={<NotAuthorized />} />
+        )}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
